990a11432f20dd4ebdb2e0c5a26943cff1ca8053
dvberkel <daan.v.berkel.1980@gmail.com>
2014-12-22 12:17:37 +0100

    specify a sprite editor model

--- spec/sprite.editorSpec.js
+++ spec/sprite.editorSpec.js
@@ -6,4 +6,99 @@ describe('sprite', function(){
 	it('should have an \'editor\'', function(){
 		expect(sprite.editor).toBeDefined();
 	});
+
+	describe('editor', function(){
+		it('should have a \'Model\'', function(){
+			expect('sprite.editor.Model').toBeDefined();
+		});
+
+		describe('Model', function(){
+			var expectedColumns;
+			var expectedRows;
+
+			beforeEach(function(){
+				expectedColumns = 4;
+				expectedRows = 3;
+			});
+
+
+			it('should set a number of columns and rows', function(){
+				var model = new sprite.editor.Model(expectedColumns, expectedRows);
+
+				expect(model.columns).toBe(4);
+				expect(model.rows).toBe(3);
+			});
+
+			it('should paint pixels', function(){
+				var model = new sprite.editor.Model(expectedColumns, expectedRows);
+
+				model.paintPixel(0,0);
+
+				expect(model.colorAt(0,0)).toBe('black');
+			});
+
+			it('should set brush color as option', function(){
+				var model = new sprite.editor.Model(expectedColumns, expectedRows, {
+					'brushColor': 'red'
+				});
+
+				model.paintPixel(0,0);
+
+				expect(model.colorAt(0,0)).toBe('red');
+			});
+
+			it('should alter brush color', function(){
+				var model = new sprite.editor.Model(expectedColumns, expectedRows);
+				model.changeBrushColor('blue')
+
+				model.paintPixel(0,0);
+
+				expect(model.colorAt(0,0)).toBe('blue');
+			});
+
+			it('should report \'none\' for uncolored pixels', function(){
+				var model = new sprite.editor.Model(expectedColumns, expectedRows);
+
+				expect(model.colorAt(0,0)).toBe('none');
+			});
+
+			describe('notifications', function(){
+				it('should notify of pixel paint', function(){
+					var notified = false;
+					var actualX;
+					var actualY;
+					var actualColor;
+					var model = new sprite.editor.Model(expectedColumns, expectedRows);
+					model.on('paint', function(x, y, color){
+						notified = true;
+						actualX = x;
+						actualY = y;
+						actualColor = color;
+					});
+
+					model.paintPixel(0, 0);
+
+					expect(notified).toBeTruthy();
+					expect(actualX).toBe(0);
+					expect(actualY).toBe(0);
+					expect(actualColor).toBe('black');
+				});
+
+				it('should notify of color change', function(){
+					var notified = false;
+					var actualColor;
+					var model = new sprite.editor.Model(expectedColumns, expectedRows);
+					model.on('color', function(color){
+						notified = true;
+						actualColor = color;
+					});
+
+					model.changeBrushColor('green');
+
+					expect(notified).toBeTruthy();
+					expect(actualColor).toBe('green');
+				});
+			});
+		});
+	});
 });
--- sprite.editor.js
+++ sprite.editor.js
@@ -1,3 +1,49 @@
-;(function(sprite){
+;(function(sprite, undefined){
+	var Observable = function(){
+		this.observers = {};
+	};
+	Observable.prototype.on = function(event, callback){
+		(this.observers[event] = this.observers[event] || []).push(callback);
+	};
+	Observable.prototype.signal = function(event){
+		var args = Array.prototype.slice.call(arguments, 1);
+		(this.observers[event] || []).forEach(function(observer){
+			observer.apply(this, args);
+		}.bind(this));
+	};
+
 	var editor = sprite.editor = {};
+
+	var defaultModelOptions = {
+		'brushColor': 'black',
+		'uncolored': 'none'
+	}
+
+	var Model = editor.Model = function(columns, rows, options){
+		Observable.call(this);
+		options = options || {};
+		for (var key in defaultModelOptions) {
+			this[key] = options[key] !== undefined ? options[key] : defaultModelOptions[key];
+		}
+		this.columns = columns;
+		this.rows = rows;
+		this.pixels = {};
+	};
+	Model.prototype = Object.create(Observable.prototype);
+	Model.prototype.constructor = Model;
+	Model.prototype.paintPixel = function(x, y){
+		var column = this.pixels[x] = this.pixels[x] || {};
+		column[y] = this.brushColor;
+		this.signal('paint', x, y, this.brushColor);
+	};
+	Model.prototype.colorAt = function(x, y){
+		if (this.pixels[x] && this.pixels[y]) {
+			return this.pixels[x][y];
+		}
+		return this.uncolored;
+	}
+	Model.prototype.changeBrushColor = function(color){
+		this.brushColor = color !== undefined ? color : defaultModelOptions.brushColor;
+		this.signal('color', this.brushColor);
+	}
 })(window.sprite = window.sprite || {});
