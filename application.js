a9699ff2e7f3bf6337076a4d70ff2e22fb287ed5
dvberkel <daan.v.berkel.1980@gmail.com>
2015-01-02 21:55:14 +0100

    use continuous draw mode

--- application.js
+++ application.js
@@ -3,6 +3,8 @@
 
 	var canvas = document.getElementById('pixel-editor');
 	var view = new sprite.editor.View(model, canvas);
+	var controller = sprite.editor.controllerFor(model, view, canvas);
 
-	canvas.addEventListener('mousedown', sprite.editor.controllerFor(model, view));
+	canvas.addEventListener('mousedown', controller.startDrawing.bind(controller));
+	canvas.addEventListener('mouseup', controller.stopDrawing.bind(controller));
 })(sprite);
